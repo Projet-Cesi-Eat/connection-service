7f132edb5af7cb2a14f77db01c9dbb2dbe441260
Louis BOSQUI <louis.bosqui@viacesi.fr>
2021-06-28 17:31:47 +0200

    Added signup and login functions, with middleware for checking JWTs

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -2,9 +2,12 @@ import { Request, Response, NextFunction } from 'express'
 const express = require('express');
 const router = express.Router();
 
+const userController = require('../Controllers/userController')
+
+
 /* GET users listing. */
-router.get('/', function (req: Request, res: Response, next: NextFunction) {
-  res.send('respond with a resource');
-});
+router.post('/signup', userController.signup);
+
+router.post('/login', userController.login);
 
 module.exports = router;
