a987034e1a7d362a95d123dfa9fd8a20a4ba69e8
Peter Giromagny <peter.giromagny@viacesi.fr>
2021-06-30 11:50:16 +0200

    Update check token with cookies

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,22 +1,29 @@
+import { Request, Response, NextFunction } from 'express';
 const MWjwt = require('jsonwebtoken');
 
-module.exports = (req: any, res: any, next: any) => {
-    try{
-        const token = req.headers.authorization.split(' ')[1];
-        const decodedToken = MWjwt.verify(token, 'SECRET_RANDOM_STRING');
-        const id_user = decodedToken.id_user;
-        const role_user = decodedToken.role_user;
-        if (req.body.id_user && req.body.id_user !== id_user) {
-            throw 'Invalid user ID';
-        }
-        else if (req.body.role && req.body.role_user !== role_user) {
-            throw 'You are not authorized to make this request';
-        } else {
-            next();
-        }
-    } catch {
-        res.status(401).json({
-            error: new Error('Invalid request!')
-        });
+module.exports = (req: Request, res: Response, next: NextFunction) => {
+  try {
+    // const token = req.headers.authorization.split(' ')[1];
+    const token = req.cookies.token || '';
+
+    if (!token) {
+      res.status(401).json('You need to login');
     }
+
+    const decodedToken = MWjwt.verify(token, 'SECRET_RANDOM_STRING');
+    const id_user = decodedToken.id_user;
+    const role_user = decodedToken.role_user;
+
+    if (req.body.id_user && req.body.id_user !== id_user) {
+      throw 'Invalid user ID';
+    } else if (req.body.role && req.body.role_user !== role_user) {
+      throw 'You are not authorized to make this request';
+    } else {
+      next();
+    }
+  } catch {
+    res.status(401).json({
+      error: new Error('Invalid request!'),
+    });
+  }
 };
